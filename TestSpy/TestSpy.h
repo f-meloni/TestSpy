62caa09bf8f82e6b61c7430602c53a8f9a33299e
Franco Meloni <franco.meloni91@gmail.com>
2018-04-03 10:28:05 +0100

    Initial Commit

---
+++ TestSpy/TestSpy.h
@@ -0,0 +1,19 @@
+//
+//  TestSpy.h
+//  TestSpy
+//
+//  Created by Franco Meloni on 03/04/2018.
+//  Copyright © 2018 Franco Meloni. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestSpy.
+FOUNDATION_EXPORT double TestSpyVersionNumber;
+
+//! Project version string for TestSpy.
+FOUNDATION_EXPORT const unsigned char TestSpyVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestSpy/PublicHeader.h>
+
+
