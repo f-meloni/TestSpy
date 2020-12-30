09f379a7d4649df8fde0a2d965ae53f618d0355a
Tsungyu Yu <ytyubox@gmail.com>
2020-12-30 19:36:30 +0800

    format the text

--- Sources/TestSpy/TestSpy/Predicates/CallstackMatcher.swift
+++ Sources/TestSpy/TestSpy/Predicates/CallstackMatcher.swift
@@ -16,45 +16,36 @@ public enum CallstackMatcher<Method: Equatable> {
     case immediatelyBefore(Method)
     case after(Method)
     case immediatelyAfter(Method)
-    
-    func predicate(method: Method) -> AnyCallstackPredicate<Method>{
+
+    func predicate(method: Method) -> AnyCallstackPredicate<Method> {
         switch self {
-            case let .times(times):
-                return AnyCallstackPredicate(predicate:
-                                              CallstackMatchers.TimesMatcher(method: method, times: times)
-                )
-            case .atLeast(times: let times):
-                return AnyCallstackPredicate(predicate:
-                                              CallstackMatchers.AtLeastMatcher(method: method, times: times)
-                )
-            case .never:
-                return AnyCallstackPredicate(predicate:
-                                              CallstackMatchers.NeverMatcher(expected: method)
-                )
-            case .any:
-                return AnyCallstackPredicate(predicate:
-                                              CallstackMatchers.AnyMatcher(method: method)
-                )
-            case .only:
-                return AnyCallstackPredicate(predicate:
-                                              CallstackMatchers.OnlyMatcher(method: method)
-                )
-            case let .before(otherMethod):
-                return AnyCallstackPredicate(predicate:
-                                              CallstackMatchers.BeforeMatcher(method: method, otherMethod: otherMethod)
-                )
-            case let .immediatelyBefore(otherMethod):
-                return AnyCallstackPredicate(predicate:
-                                              CallstackMatchers.ImmediatelyBeforeMatcher(method: method, otherMethod: otherMethod)
-                )
-            case let .after(otherMethod):
-                return AnyCallstackPredicate(predicate:
-                                              CallstackMatchers.AfterMatcher(method: method, otherMethod: otherMethod)
-                )
-            case let .immediatelyAfter(otherMethod):
-                return AnyCallstackPredicate(predicate:
-                                              CallstackMatchers.ImmediatelyAfterMatcher(method: method, otherMethod: otherMethod)
-                )
+        case let .times(times):
+            return AnyCallstackPredicate(predicate: CallstackMatchers.TimesMatcher(method: method, times: times)
+            )
+        case let .atLeast(times: times):
+            return AnyCallstackPredicate(predicate: CallstackMatchers.AtLeastMatcher(method: method, times: times)
+            )
+        case .never:
+            return AnyCallstackPredicate(predicate: CallstackMatchers.NeverMatcher(expected: method)
+            )
+        case .any:
+            return AnyCallstackPredicate(predicate: CallstackMatchers.AnyMatcher(method: method)
+            )
+        case .only:
+            return AnyCallstackPredicate(predicate: CallstackMatchers.OnlyMatcher(method: method)
+            )
+        case let .before(otherMethod):
+            return AnyCallstackPredicate(predicate: CallstackMatchers.BeforeMatcher(method: method, otherMethod: otherMethod)
+            )
+        case let .immediatelyBefore(otherMethod):
+            return AnyCallstackPredicate(predicate: CallstackMatchers.ImmediatelyBeforeMatcher(method: method, otherMethod: otherMethod)
+            )
+        case let .after(otherMethod):
+            return AnyCallstackPredicate(predicate: CallstackMatchers.AfterMatcher(method: method, otherMethod: otherMethod)
+            )
+        case let .immediatelyAfter(otherMethod):
+            return AnyCallstackPredicate(predicate: CallstackMatchers.ImmediatelyAfterMatcher(method: method, otherMethod: otherMethod)
+            )
         }
     }
 }
--- Sources/TestSpy/TestSpy/Predicates/CallstackVerifyer.swift
+++ Sources/TestSpy/TestSpy/Predicates/CallstackVerifyer.swift
@@ -1,24 +1,23 @@
 //
-/* 
+/*
  *		Created by 游宗諭 in 2020/12/30
- *		
+ *
  *		Using Swift 5.0
- *		
+ *
  *		Running on macOS 10.15
  */
 
-
-//import Foundation
-public struct CallStackVerifyer<Method: Equatable>: CallstackPredicate{
+// import Foundation
+public struct CallStackVerifyer<Method: Equatable>: CallstackPredicate {
     init(verifyer: @escaping (Method) -> Bool) {
         self.verifyer = verifyer
     }
-    let verifyer:(Method) -> Bool
-    
+
+    let verifyer: (Method) -> Bool
+
     public func verify(method: Method) -> Bool {
         verifyer(method)
     }
-    
 
     public func check(against callstack: [Method]) -> Bool {
         callstack.allSatisfy {
@@ -26,13 +25,13 @@ public struct CallStackVerifyer<Method: Equatable>: CallstackPredicate{
         }
     }
 
-
     public func description(forMethod method: Method) -> String {
         "have received \(method)"
     }
 }
+
 public extension TestSpy {
-    func check(verifeyer: @escaping (Method) -> Bool ) -> Bool {
+    func check(verifeyer: @escaping (Method) -> Bool) -> Bool {
         return CallStackVerifyer(verifyer: verifeyer).check(against: callstack.callstack)
     }
 }
