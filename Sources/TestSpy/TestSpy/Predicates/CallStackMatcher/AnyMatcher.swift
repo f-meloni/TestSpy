6daacda71dd8973480d95f768e42938eca7176ae
Tsungyu Yu <ytyubox@gmail.com>
2020-12-30 19:55:35 +0800

    typo file name mistake

---
+++ Sources/TestSpy/TestSpy/Predicates/CallStackMatcher/AnyMatcher.swift
@@ -0,0 +1,25 @@
+//
+/*
+ *		Created by 游宗諭 in 2020/12/30
+ *
+ *		Using Swift 5.0
+ *
+ *		Running on macOS 10.15
+ */
+public extension CallstackMatchers {
+    struct AnyMatcher<Method: Equatable>: CallstackPredicate {
+        let method: Method
+
+        public func verify(method: Method) -> Bool {
+            self.method == method
+        }
+
+        public func check(against callstack: [Method]) -> Bool {
+            callstack.contains(method)
+        }
+
+        public func description(forMethod method: Method) -> String {
+            "have received \(method)"
+        }
+    }
+}
